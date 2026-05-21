e11445fdddef7924a47d17c40fbb296c2af71077
jkbSeven <Jacob202@protonmail.com>
2026-05-21 15:05:07 +0200

    basic pipeline

---
+++ backend/scripts/pipeline.sh
@@ -0,0 +1,16 @@
+SCRIPTS_DIR=$(dirname $0)
+
+run() {
+    if [ ! -x $1 ]; then
+        printf 'Script %s does not exist or is not executable\n' "$1" 1>&2
+        exit 1
+    fi
+
+    CMD="sh $1"
+
+    printf '=== RUN: %s ===\n' "$CMD"
+    $CMD || exit $?
+}
+
+run "${SCRIPTS_DIR}/fmt.sh" || exit $?
+run "${SCRIPTS_DIR}/lint.sh" || exit $?
