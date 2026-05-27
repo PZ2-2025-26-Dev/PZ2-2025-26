913bab3f4e01a6acf7968c5a0bba4e91e3f92346
jkbSeven <Jacob202@protonmail.com>
2026-05-27 18:17:53 +0200

    added location models

---
+++ backend/src/locations/constants.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class LocationType(Enum):
+    BUILDING = "building"
+    ROOM = "room"
+    CABINET = "cabinet"
+    SHELF = "shelf"
---
+++ backend/src/locations/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Enum, ForeignKey, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database import Base
+from src.locations.constants import LocationType
+
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    type: Mapped[LocationType] = mapped_column(Enum(LocationType), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text)
+
+    # "RESTRICT" żeby nie usunąć przypadkowo rodzica
+    # najpierw trzeba przenieść dzieci do innego rodzica, potem dopiero można usunąć
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey("locations.id", ondelete="RESTRICT"))
+
+    # To pomaga w recursive CTE
+    children: Mapped[list["Location"]] = relationship("Location", back_populates="parent")
+    parent: Mapped["Location | None"] = relationship("Location", back_populates="children", remote_side=[id])
