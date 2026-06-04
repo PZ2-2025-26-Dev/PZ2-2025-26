0ed40bc23302e386958f9b6def5fd88b9c440c31
jkbSeven <Jacob202@protonmail.com>
2026-06-04 17:17:23 +0200

    added dummy unit test

---
+++ backend/tests/test_dummy.py
@@ -0,0 +1,9 @@
+# workaround:
+# dopóki nie mamy innych testów jednostkowych
+# potrzebujemy jakiś prosty test, który nie jest oznaczony "integration"
+# żeby `./scripts/pipeline.sh` działało poprawnie
+# Jak dodamy inne testy jednostkowe to możemy ten usunąć
+
+
+def test_add():
+    assert 2 + 2 == 4
