21604f9c0eeae9449b213a43a38be4a59d5a1846
jkbSeven <Jacob202@protonmail.com>
2026-06-04 02:29:36 +0200

    improved the db-intgr script to run docker compose down upon exit action (using "trap"); sourcing .env instead of grepping

--- backend/scripts/db-intgr-tests.sh
+++ backend/scripts/db-intgr-tests.sh
@@ -8,21 +8,15 @@ check_cmd grep
 check_cmd sed
 check_cmd uv
 
+trap 'docker compose --file ${PZ_BACKEND_DIR?}/compose.yaml down && docker volume rm backend_mysql_data' EXIT
+
 if [ ! -f "${PZ_BACKEND_DIR?}/.env" ]; then
     error ".env file is missing! (looking in ${PZ_BACKEND_DIR?})"
 fi
 
-if ! DB_URL=$(grep "PZ_DATABASE_URL" .env); then
-    error ".env file is missing the PZ_DATABASE_URL variable"
-fi
-
-DB_URL=${DB_URL#*=}  # remove everything before '=' (including)
-DB_URL=$(printf '%s\n' "$DB_URL" | sed -e 's/@db/@localhost/') || exit $? # replace '@db' with '@localhost'
-
 docker compose --file "${PZ_BACKEND_DIR?}/compose.yaml" up --build --detach || exit $?
 
 API_READY=0
-
 for i in 1 2 3 4 5; do
 
     printf 'Waiting for API to be ready (%d/5)...' $i
@@ -39,10 +33,16 @@ for i in 1 2 3 4 5; do
 done
 
 if [ $API_READY -ne 1 ]; then
-    error 'exceeded wait time for API startup (~50s)'
+    error 'exceeded wait time for API startup'
 fi
 
-PZ_DATABASE_URL="$DB_URL" uv run python -m pytest -vv "${PZ_BACKEND_DIR?}/tests" || exit $?
+. "${PZ_BACKEND_DIR?}/.env" || exit $?
 
-docker compose down || exit $?
-docker volume rm backend_mysql_data || exit $?
+if [ -z "$PZ_DATABASE_URL" ]; then
+    error "variable PZ_DATABASE_URL is not set!"
+fi
+
+PZ_DATABASE_URL=$(printf '%s\n' "${PZ_DATABASE_URL?}" | sed -e 's/@db/@localhost/') || exit $?
+export PZ_DATABASE_URL
+
+uv run python -m pytest -vv "${PZ_BACKEND_DIR?}/tests" || exit $?
